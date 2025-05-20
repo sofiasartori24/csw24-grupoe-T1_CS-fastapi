d60e74748d1925ff5f1f1a2ecae6a43861e5391a
DudaWendelMaia <dudawendelmaia@gmail.com>
2025-05-20 18:45:55 -0300

    files terraform

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "allow_flask" {
+  name        = "allow_flask"
+  description = "Permitir tráfego na porta 8080"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "flask_instance" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_flask.id]
+
+  tags = {
+    Name = "FlaskServerInstance"
+  }
+}
