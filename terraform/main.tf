753f79dab4044b0c9589773cde426af6f98df9d3
Isadora Ferreira Guerra <isadora.guerra@edu.pucrs.br>
2025-06-01 16:19:34 -0300

    Atualiza terraform/main sem expor credenciais

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,57 @@
-resource "aws_security_group" "allow_flask" {
-  name        = "allow_flask"
-  description = "Permitir tráfego na porta 8080"
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_exec_role"
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
 }
 
-resource "aws_instance" "flask_instance" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.allow_flask.id]
-
-  tags = {
-    Name = "FlaskServerInstance"
-  }
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "my_lambda_function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.12"
+  filename      = "lambda_function.zip"
+  source_code_hash = filebase64sha256("lambda_function.zip")
 }
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.my_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+}
+
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "my-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id           = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.my_lambda.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "default" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "ANY /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+//instancia rds de mysql
+//gerar um template do cloudFormation para api labda
+//comando aws sam
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "ami_id" {
   description = "ID da imagem AMI"
-  default     = "ami-0c55b159cbfafe1f0"  # Ubuntu Server 20.04 LTS (us-east-1)
+  default     = "ami-0c55b159cbfafe1f0"
 }
 
 variable "instance_type" {
@@ -9,5 +9,5 @@ variable "instance_type" {
 
 variable "key_name" {
   description = "Nome da chave SSH"
-  default     = "minha-chave-ssh"  # Substitua pelo nome da sua chave EC2
+  default     = "construcao"
 }
