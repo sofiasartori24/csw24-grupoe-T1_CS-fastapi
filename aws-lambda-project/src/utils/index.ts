f9a04879c5269d7e8aa52b3a5f2558b7d707850b
Isadora Ferreira Guerra <isadora.guerra@edu.pucrs.br>
2025-06-01 18:33:04 -0300

    new lambda in python

--- aws-lambda-project/src/utils/index.ts
+++
@@ -1,9 +0,0 @@
-export const processData = (data: any): any => {
-    // Implement data processing logic here
-    return data;
-};
-
-export const validateInput = (input: any): boolean => {
-    // Implement input validation logic here
-    return true;
-};
