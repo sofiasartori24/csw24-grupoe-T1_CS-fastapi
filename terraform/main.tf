9f2f70da23556de6c74a22148b6e5c07f53905e8
Isadora Ferreira Guerra <isadora.guerra@edu.pucrs.br>
2025-06-01 17:10:18 -0300

    fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,57 +1,56 @@
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_exec_role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
+resource "aws_db_subnet_group" "default" {
+  name       = "rds-subnet-group"
+  subnet_ids = var.private_subnet_ids
 }
 
-resource "aws_lambda_function" "my_lambda" {
-  function_name = "my_lambda_function"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.12"
-  filename      = "lambda_function.zip"
-  source_code_hash = filebase64sha256("lambda_function.zip")
+resource "aws_security_group" "rds_sg" {
+  name   = "rds_sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    # A SAS da Lambda vai no Security Group a seguir
+    security_groups = [aws_security_group.lambda_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.my_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
+resource "aws_security_group" "lambda_sg" {
+  name   = "lambda_sg"
+  vpc_id = var.vpc_id
 }
 
-resource "aws_apigatewayv2_api" "http_api" {
-  name          = "my-http-api"
-  protocol_type = "HTTP"
+resource "aws_db_instance" "mydb" {
+  identifier             = "meu-db-instance"
+  allocated_storage      = 20
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+
+  db_name                = var.db_name       # ← use db_name, não name
+  username               = var.db_username
+  password               = var.db_password
+
+  parameter_group_name   = "default.mysql8.0"
+  db_subnet_group_name   = aws_db_subnet_group.default.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  deletion_protection    = false
+  backup_retention_period = 7
 }
 
-resource "aws_apigatewayv2_integration" "lambda" {
-  api_id           = aws_apigatewayv2_api.http_api.id
-  integration_type = "AWS_PROXY"
-  integration_uri  = aws_lambda_function.my_lambda.invoke_arn
-}
-
-resource "aws_apigatewayv2_route" "default" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "ANY /{proxy+}"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
-}
-
-resource "aws_apigatewayv2_stage" "default" {
-  api_id      = aws_apigatewayv2_api.http_api.id
-  name        = "$default"
-  auto_deploy = true
-}
 
 //instancia rds de mysql
 //gerar um template do cloudFormation para api labda
 //comando aws sam
+// queremos Usar Terraform só para RDS e SAM/CloudFormation para Lambda + API
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,3 +11,33 @@ variable "key_name" {
   description = "Nome da chave SSH"
   default     = "construcao"
 }
+
+# variables.tf
+
+variable "vpc_id" {
+  description = "ID da VPC onde RDS e Lambda rodarão"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "Lista de IDs das subnets privadas onde RDS e Lambda ficarão"
+  type        = list(string)
+}
+
+variable "db_name" {
+  description = "Nome do banco de dados MySQL"
+  type        = string
+  default     = "resources_management"
+}
+
+variable "db_username" {
+  description = "Usuário MySQL"
+  type        = string
+  default     = "user"
+}
+
+variable "db_password" {
+  description = "Senha do usuário MySQL"
+  type        = string
+  default     = "password"
+}
