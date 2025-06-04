1c20276d7eec84f596f623e93792cdd18c2c841b
sofia sartori <sofia.sartori@poatek.com>
2025-06-04 14:39:18 -0300

    fix using existing lambda function

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,35 +41,42 @@ data "aws_db_instance" "existing" {
   db_instance_identifier = "resources-management-db"
 }
 
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/../t1_cs"
-  output_path = "${path.module}/lambda_function.zip"
+# Use existing Lambda function instead of creating a new one
+data "aws_lambda_function" "existing_lambda" {
+  function_name = "FastAPIApplication"
 }
 
-resource "aws_lambda_function" "fastapi_lambda" {
-  function_name    = "FastAPIApplication"
-  filename         = data.archive_file.lambda_zip.output_path
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  role             = "arn:aws:iam::030764292549:role/LabRole"
-  handler          = "lambda_handler.handler"
-  runtime          = "python3.9"
-  memory_size      = 512
-  timeout          = 60
+# Keep this for reference, but don't use it to create a new Lambda
+# data "archive_file" "lambda_zip" {
+#   type        = "zip"
+#   source_dir  = "${path.module}/../t1_cs"
+#   output_path = "${path.module}/lambda_function.zip"
+# }
 
-  environment {
-    variables = {
-      # Use environment variables for database connection
-      # since we're using an existing RDS instance
-      DB_HOST     = var.db_host
-      DB_USER     = var.db_username
-      DB_PASSWORD = var.db_password
-      DB_NAME     = var.db_name
-    }
-  }
-
-  # No dependencies on IAM resources since we're using an existing role
-}
+# Use existing Lambda function instead of creating a new one
+# resource "aws_lambda_function" "fastapi_lambda" {
+#   function_name    = "FastAPIApplication"
+#   filename         = data.archive_file.lambda_zip.output_path
+#   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+#   role             = "arn:aws:iam::030764292549:role/LabRole"
+#   handler          = "lambda_handler.handler"
+#   runtime          = "python3.9"
+#   memory_size      = 512
+#   timeout          = 60
+#
+#   environment {
+#     variables = {
+#       # Use environment variables for database connection
+#       # since we're using an existing RDS instance
+#       DB_HOST     = var.db_host
+#       DB_USER     = var.db_username
+#       DB_PASSWORD = var.db_password
+#       DB_NAME     = var.db_name
+#     }
+#   }
+#
+#   # No dependencies on IAM resources since we're using an existing role
+# }
 
 # API Gateway Resources
 resource "aws_apigatewayv2_api" "api_gateway" {
@@ -86,7 +93,7 @@ resource "aws_apigatewayv2_stage" "api_stage" {
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id             = aws_apigatewayv2_api.api_gateway.id
   integration_type   = "AWS_PROXY"
-  integration_uri    = aws_lambda_function.fastapi_lambda.invoke_arn
+  integration_uri    = data.aws_lambda_function.existing_lambda.invoke_arn
   integration_method = "POST"
   payload_format_version = "2.0"
 }
@@ -106,7 +113,7 @@ resource "aws_apigatewayv2_route" "root_route" {
 resource "aws_lambda_permission" "api_gateway_permission" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.fastapi_lambda.function_name
+  function_name = data.aws_lambda_function.existing_lambda.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/*"
 }
